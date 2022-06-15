6a4d8354ef33999136eaa65831ac1a9b95575c25
Akshay Ganeshen <me@aganeshen.com>
2022-06-15 17:20:18 -0400

    feat(js_native_api): add GetUndefined

---
+++ js_native_api.go
@@ -0,0 +1,20 @@
+package napi
+
+/*
+#include <stdlib.h>
+#include <node/node_api.h>
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+func GetUndefined(env Env) (Value, Status) {
+	var result Value
+	status := Status(C.napi_get_undefined(
+		C.napi_env(env),
+		(*C.napi_value)(unsafe.Pointer(&result)),
+	))
+	return result, status
+}
